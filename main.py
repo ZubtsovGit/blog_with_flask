e56707fee0b7d1887ab62c6672bb8f7ed459bc99
ZubtsovGit <dania.zubtsov@gmail.com>
2023-11-08 12:03:27 +0300

    routing and data fetching added

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, jsonify, render_template
+import requests
 
+
+def data_from_api():
+    if response.status_code == 200:
+        data = response.json
+        return data
+    else:
+        return jsonify({"error": "Failed to fetch data from API"}), 500
+
+
+api_url = 'https://api.npoint.io/4e69431118e2881acf61'
+response = requests.get(api_url)
+all_posts = data_from_api()
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', posts=all_posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
