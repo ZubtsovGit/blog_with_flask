f04431e7f2d5c50acd309d0c2d46cdf2fd0b95cc
ZubtsovGit <dania.zubtsov@gmail.com>
2023-11-10 13:18:15 +0300

    individual posts are added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 
 def data_from_api():
     if response.status_code == 200:
-        data = response.json
+        data = response.json()
         return data
     else:
         return jsonify({"error": "Failed to fetch data from API"}), 500
@@ -30,6 +30,16 @@ def about():
 def contact():
     return render_template('contact.html')
 
+@app.route("/post/<title>")
+def get_post(title):
+    requested_post = None
+    for blog_post in all_posts:
+        print(blog_post, type(blog_post))
+
+        if blog_post['title'] == title:
+            requested_post = blog_post
+
+    return render_template("post.html", post=requested_post)
 
 if __name__ == '__main__':
     app.run(debug=True)
